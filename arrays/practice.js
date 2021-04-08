c465f96eb757a24ca0a03b2491ead075e7130d47
kaley forster <55896549+kale-redhead@users.noreply.github.com>
2021-04-08 10:23:36 -0400

    worked on arrays problems

--- arrays/practice.js
+++ arrays/practice.js
@@ -17,7 +17,9 @@ var arr = [10,20,30];
   Return the first item in the given array.
 */
 
-//Code Here
+function first(arr){
+  return arr[0];
+}
 
 
 
@@ -32,8 +34,9 @@ var arr = [40,50,60];
   Return the last item in the given array.
 */
 
-//Code Here
-
+function last(arr){
+  return arr[2];
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -47,7 +50,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
   Loop through the given array and alert every item in the array.
 */
 
-//Code Here
+function looper(family){
+  for (let i = 0; i <= family.length; i++){
+    console.log(family[i]);
+  }
+}
 
 
 
@@ -62,8 +69,9 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
   Loop backwards, starting at the end of the letters array, alerting every item in the array.
 */
 
-//Code Here
-
+function reversedLooper(){
+  
+}
 
 
 ////////// PROBLEM 5 //////////
@@ -77,7 +85,9 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
   Return an array that contains the even numbers from the nums array.
 */
 
-//Code Here
+function evenFinder(){
+
+}
 
 
 
